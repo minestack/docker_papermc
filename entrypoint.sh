3e81454189d8746dbda2dba597cbfe9f3ff7e339
Ryan Belgrave <ryan@belgrave.me>
2022-02-14 18:13:51 -0600

    use env vars for java args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/dumb-init /bin/bash
+set -xe
 
-java -Xms2G -Xmx2G -Dlog4j.configurationFile=log4j2.xml -jar paper.jar --nogui -W worlds --nojline
+java -Xms${JVM_HEAP} -Xmx${JVM_HEAP} -Dlog4j.configurationFile=log4j2.xml ${JAVA_ARGS} -jar paper.jar --nogui -W worlds --nojline
